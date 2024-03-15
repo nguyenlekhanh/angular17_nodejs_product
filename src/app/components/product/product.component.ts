8f4c317c8636e6d599eaa7ec1d1a4554716d325f
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-15 16:51:17 -0500

    add product detail, rating and import RatingModule from primeng/rating

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,10 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../../types';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'primeng/rating';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RatingModule,
+  ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
