73341f75153b28db9032d9e64cc709122f509d56
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-15 21:57:50 -0500

    adding function add, create, delete product to UI, install primeicons

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,8 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { Product } from '../../../types';
 import { FormsModule } from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
 import { CommonModule } from '@angular/common';
+import { ConfirmationService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-product',
@@ -11,10 +15,39 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     FormsModule,
     RatingModule,
+    ButtonModule,
+    ConfirmPopupModule,
+    ToastModule
   ],
+  providers: [ConfirmationService],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
+  constructor(private confirmationService: ConfirmationService) {}
+
+  @ViewChild('deleteButton') deleteButton: any;
+
   @Input() product!: Product;
+
+  @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
+  @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
+
+  editProduct() {
+    this.edit.emit(this.product);
+  }
+
+  confirmDelete() {
+    this.confirmationService.confirm({
+      target: this.deleteButton.nativeElement,
+      message: 'Are you sure that you want to delete this product?',
+      accept: () => {
+        this.deleteProduct();
+      },
+    });
+  }
+
+  deleteProduct() {
+    this.delete.emit(this.product);
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,8 @@ import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
 import { Paginator, PaginatorModule } from 'primeng/paginator';
+import { EditPopupComponent } from '../components/edit-popup/edit-popup.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +13,9 @@ import { Paginator, PaginatorModule } from 'primeng/paginator';
   imports: [
     CommonModule,
     ProductComponent,
-    PaginatorModule
+    PaginatorModule,
+    EditPopupComponent,
+    ButtonModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
@@ -29,6 +33,52 @@ export class HomeComponent {
   totalRecords: number = 0;
   rows: number = 12;
 
+  displayEditPopup: boolean = false;
+  displayAddPopup: boolean = false;
+
+  toggleEditPopup(product: Product) {
+    this.selectedProduct = product;
+    this.displayEditPopup = true;
+  }
+
+  toggleDeletePopup(product: Product) {
+    if (!product.id) {
+      return;
+    }
+
+    this.deleteProduct(product.id);
+  }
+
+  toggleAddPopup() {
+    this.displayAddPopup = true;
+  }
+
+  selectedProduct: Product = {
+    id: 0,
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+  };
+
+  onConfirmEdit(product: Product) {
+    if (!this.selectedProduct.id) {
+      return;
+    }
+
+    this.editProduct(product, this.selectedProduct.id);
+    this.displayEditPopup = false;
+  }
+
+  onConfirmAdd(product: Product) {
+    this.addProduct(product);
+    this.displayAddPopup = false;
+  }
+
+  onProductOutput(product: Product) {
+    console.log(product, 'Output');
+  }
+
   onPageChange(event: any) {
     this.fetchProducts(event.page, event.rows);
   }
@@ -36,7 +86,7 @@ export class HomeComponent {
   resetPaginator() {
     this.paginator?.changePage(0);
   }
-  
+
   fetchProducts(page: number, perPage: number) {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page, perPage })
