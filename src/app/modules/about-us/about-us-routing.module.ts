ec539d6c94e5171fe1666b1c99b2b08acfeb0f23
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-15 22:48:57 -0500

    add another child route for learning purpose

--- src/app/modules/about-us/about-us-routing.module.ts
+++ src/app/modules/about-us/about-us-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { AboutUs2Component } from './about-us-2/about-us-2.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AboutUsComponent
+  },
+  {
+    path: 'about-us-2',
+    component: AboutUs2Component
   }
 ];
 
--- src/app/modules/about-us/about-us.module.ts
+++ src/app/modules/about-us/about-us.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AboutUsRoutingModule } from './about-us-routing.module';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { AboutUs2Component } from './about-us-2/about-us-2.component';
 
 
 @NgModule({
   declarations: [
-    AboutUsComponent
+    AboutUsComponent,
+    AboutUs2Component
   ],
   imports: [
     CommonModule,
